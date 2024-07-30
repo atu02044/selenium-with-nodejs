28df4e4c19dda42e2a4be8ce24010a18e3e9e5a6
chautu0204@gmail.com <chautu0204@gmail.com>
2024-07-30 22:53:10 +0700

    test nha

--- testScript/script01.js
+++ testScript/script01.js
@@ -1,5 +1,5 @@
 describe('Test suite 01', function(){
     it('test case 01', function(){
-        browser.url('https://google.com')
+        browser.url('https://the-internet.herokuapp.com/')
     })
 });
